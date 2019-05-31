93af898c84ee0a17140a724473fa4a7935a27653
HamletGalstyan <51154878+HamletGalstyan@users.noreply.github.com>
2019-05-31 17:31:59 +0400

    Add files via upload

---
+++ Singletone/SingleTone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTone
+{
+    class SingleTone
+    {
+        public static SingleTone @object { get; set; }
+
+        private SingleTone()
+        {
+
+        }
+
+        public static SingleTone SingleObject()
+        {
+            if (@object == null)
+            {
+                @object = new SingleTone();
+            }
+            return @object;
+        }
+    }
+}
