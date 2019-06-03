de1a5943103148010e066a0297153047920adfa4
HamletGalstyan <51154878+HamletGalstyan@users.noreply.github.com>
2019-06-03 15:08:05 +0400

    Add files via upload

---
+++ Factory method/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Developer dev = new PanelDeveloper(" ");
+            House house2 = dev.Create();
+
+            dev = new WoodDeveloper("Private Developer");
+            House house = dev.Create();
+
+            Console.ReadLine();
+        }
+    }
+
+
+    abstract class Developer
+    {
+        public string Name { get; set; }
+        public Developer(string n)
+        {
+            Name = n;
+        }
+        abstract public House Create();
+    }
+
+    class PanelDeveloper : Developer
+    {
+        public PanelDeveloper(string n) : base(n)
+        { }
+
+        public override House Create()
+        {
+            return new PanelHouse();
+        }
+
+    }
+
+    class WoodDeveloper : Developer
+    {
+        public WoodDeveloper(string n) : base(n)
+        { }
+
+        public override House Create()
+        {
+            return new WoodHouse();
+        }
+    }
+
+    abstract class House
+    { }
+
+    class PanelHouse : House
+    {
+        public PanelHouse()
+        {
+            Console.WriteLine("Panel home is created");
+        }
+    }
+    class WoodHouse : House
+    {
+        public WoodHouse()
+        {
+            Console.WriteLine("Wood house is created");
+        }
+    }
+}
+
+
+
+    
